eeb9410dbdde25e5643c237aa9697f74dbcf735f
Stephen Finucane <stephen@that.guru>
2026-03-06 15:21:07 +0000

    Remove references to unittest2
    unittest2 has not had a release in a decade [1].
    
    [1] https://pypi.org/project/unittest2/
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- testresources/tests/test_optimising_test_suite.py
+++ testresources/tests/test_optimising_test_suite.py
@@ -22,11 +22,6 @@ from testresources import split_by_resources
 from testresources.tests import ResultWithResourceExtensions
 import unittest
 
-try:
-    import unittest2
-except ImportError:
-    unittest2 = None
-
 
 def test_suite():
     from testresources.tests import TestUtil
@@ -104,15 +99,6 @@ class TestOptimisingTestSuite(testtools.TestCase):
         # that suite.
         case = self.makeTestCase()
         suite = unittest.TestSuite([case])
-        self.optimising_suite.addTest(suite)
-        self.assertEqual([case], self.optimising_suite._tests)
-
-    @testtools.skipIf(unittest2 is None, "Unittest2 needed")
-    def testAddUnittest2TestSuite(self):
-        # Adding a unittest2 test suite is the same as adding all the tests in
-        # that suite.
-        case = self.makeTestCase()
-        suite = unittest2.TestSuite([case])
         self.optimising_suite.addTest(suite)
         self.assertEqual([case], self.optimising_suite._tests)
 
