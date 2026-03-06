50d2c3519c591a5a73fdbf0c85bc426236fcd9ac
Stephen Finucane <stephen@that.guru>
2026-03-06 13:53:17 +0000

    Migrate to hatch
    The only meaningful change is that we now set requires-python, but we
    haven't supported Python 2 is a long time so that's not a big issue.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- setup.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-import setuptools
-
-setuptools.setup(setup_requires=["pbr>=1.3"], pbr=True)
--- testresources/__init__.py
+++ testresources/__init__.py
@@ -21,10 +21,7 @@ import heapq
 import inspect
 import unittest
 
-try:
-    from collections.abc import MutableSet
-except ImportError:
-    from collections import MutableSet
+from collections.abc import MutableSet
 
 # same format as sys.version_info: "A tuple containing the five components of
 # the version number: major, minor, micro, releaselevel, and serial. All
@@ -38,11 +35,56 @@ except ImportError:
 # If the releaselevel is 'final', then the tarball will be major.minor.micro.
 # Otherwise it is major.minor.micro~$(revno).
 
-from pbr.version import VersionInfo
 
-_version = VersionInfo("testresources")
-__version__ = _version.semantic_version().version_tuple()
-version = _version.release_string()
+def __get_git_version() -> str | None:
+    import os
+    import subprocess
+
+    cwd = os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)
+
+    try:
+        out = subprocess.check_output(
+            ["git", "describe"], stderr=subprocess.STDOUT, cwd=cwd
+        )
+    except (OSError, subprocess.CalledProcessError):
+        return None
+
+    try:
+        version = out.strip().decode("utf-8")
+    except UnicodeDecodeError:
+        return None
+
+    if "-" in version:  # after tag
+        # convert version-N-githash to version.postN+githash
+        return version.replace("-", ".post", 1).replace("-g", "+git", 1)
+    else:
+        return version
+
+
+# same format as sys.version_info: "A tuple containing the five components of
+# the version number: major, minor, micro, releaselevel, and serial. All
+# values except releaselevel are integers; the release level is 'alpha',
+# 'beta', 'candidate', or 'final'. The version_info value corresponding to the
+# Python version 2.0 is (2, 0, 0, 'final', 0)."  Additionally we use a
+# releaselevel of 'dev' for unreleased under-development code.
+#
+# If the releaselevel is 'alpha' then the major/minor/micro components are not
+# established at this point, and setup.py will use a version of next-$(revno).
+# If the releaselevel is 'final', then the tarball will be major.minor.micro.
+# Otherwise it is major.minor.micro~$(revno).
+
+try:
+    from ._version import __version__, version
+except ModuleNotFoundError:
+    # package is not installed
+    if v := __get_git_version():
+        version = v
+        # we're in a git repo
+        __version__ = tuple([int(x) if x.isdigit() else x for x in version.split(".")])
+    else:
+        # we're working with a tarball or similar
+        version = "0.0.0"
+        __version__ = (0, 0, 0)
 
 
 def test_suite():
